3cc553cfad4f9e24534e7f8594d81bfced6b7c86
Ablay19 <Ablay19@github.com>
2025-11-15 19:51:31 +0000

    Backupped!

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -1,4 +1,4 @@
 !#/data/data/com.termux/files/usr/bin/bash
-pkg i carapace zoxide nushell fzf neovim atuin jq ripgrep stow -y
+pkg i gh carapace zoxide nushell fzf neovim atuin jq ripgrep stow -y
 stow . --adopt
 atuin login -u Ablay19 -p 19020735@ --key 'purpose dolphin list gasp powder income riot gentle demise feature dry mixed voyage letter obvious daughter window six small slam very effort cover winner'

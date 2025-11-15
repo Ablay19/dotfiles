ac9463cbc2241bb8554b31168891173c3abcfbc3
Ablay19 <Ablay19@github.com>
2025-11-15 20:09:44 +0000

    Backupped!

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 !#/data/data/com.termux/files/usr/bin/bash
 pkg i gh carapace zoxide nushell fzf neovim atuin jq ripgrep stow -y
 stow . --adopt
-atuin login -u Ablay19 -p 19020735@ --key 'purpose dolphin list gasp powder income riot gentle demise feature dry mixed voyage letter obvious daughter window six small slam very effort cover winner'
